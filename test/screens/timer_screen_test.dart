58c9794a8bbedc140b2e9745eaf7104d177e9113
Matthew Schaney <matt@schaney.net>
2025-09-06 15:37:46 -0400

    MIND-7: Create timer_screen_test.dart

---
+++ test/screens/timer_screen_test.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:eleumind/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  group('TimerScreen', () {
+    testWidgets('shows 00:00 and Idle initially', (tester) async {
+      await tester.pumpWidget(const EleuMindApp());
+      expect(find.text('00:00'), findsOneWidget);
+      expect(find.text('Idle'), findsOneWidget);
+    });
+
+    testWidgets('goes to Running when Start tapped', (tester) async {
+      await tester.pumpWidget(const EleuMindApp());
+      await tester.tap(find.text('Start'));
+      await tester.pump();
+      expect(find.text('Running'), findsOneWidget);
+    });
+
+    testWidgets('goes Paused then back to Idle via Stop', (tester) async {
+      await tester.pumpWidget(const EleuMindApp());
+      await tester.tap(find.text('Start'));
+      await tester.pump();
+      await tester.tap(find.text('Pause'));
+      await tester.pump();
+      await tester.tap(find.text('Stop'));
+      await tester.pump();
+      expect(find.text('Idle'), findsOneWidget);
+    });
+  });
+}
